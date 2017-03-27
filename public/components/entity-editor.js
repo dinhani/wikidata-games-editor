5a0ec1a29b1b81b3db90561996e712b27fb8a423
Renato Dinhani Conceição <renatodinhani@gmail.com>
2017-03-27 04:42:01 -0300

    Added copy entities option in the editor

--- public/components/entity-editor.js
+++ public/components/entity-editor.js
@@ -3,8 +3,24 @@
 // =============================================================================
 app.controller('EntityEditorCtrl', function ($scope, client) {
     // =========================================================================
-    //
+    // DATA
     // =========================================================================
+    $scope.data = {
+        sourceEntities: []
+    }
+
+    // =========================================================================
+    // METHODS
+    // =========================================================================
+    $scope.copySourceEntities = function (properties) {
+        $scope.data.sourceEntities.forEach(entity => {
+            let notContains = _.filter(properties, p => p.id === entity.id).length === 0;
+            if (notContains) {
+                properties.push(angular.copy(entity));
+            }
+        })
+    }
+
     $scope.save = function () {
         // check which entities needs saving
         $scope.$ctrl.entities.forEach(entity => {
@@ -30,20 +46,29 @@ app.controller('EntityEditorCtrl', function ($scope, client) {
 app.component('entityEditor', {
     template: `
         <h2 class="ui top attached header">Results</h2>
-        <table class="ui attached very compact striped table">
+
+        <div class="ui attached segment">
+            <entity-selector entities="data.sourceEntities" property="$ctrl.property"></entity-selector>
+            Click "Copy" in each row to add the {{$ctrl.property.name.toLowerCase()}} from this field to the selected row
+        </div>
+
+        <table class="ui attached very compact striped center aligned table">
             <thead>
-                <th class="three wide">Entity</td>
-                <th class="ten wide">{{$ctrl.property.name}}</th>
+                <th class="two wide">Entity</td>
+                <th class="nine wide">{{$ctrl.property.name}}</th>
+                <th class="one wide">Actions</th>
                 <th class="three wide">References</th>
             </thead>
             <tbody>
                 <tr ng-repeat="entity in $ctrl.entities">
-                    <td>
+                    <td class="left aligned">
                         {{entity.name}}
                     </td>
                     <td>
                         <entity-selector entities="entity.properties" property="$ctrl.property"></entity-selector>
                     </td>
+                    <td>
+                        <a href="#" ng-click="copySourceEntities(entity.properties)">Copy</a></td>
                     <td>
                         <a ng-href="https://www.wikidata.org/wiki/{{entity.id}}" target="_blank">Wikidata</a> |
                         <a ng-href="{{entity.link}}" target="_blank">Wikipedia</a>
